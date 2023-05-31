d789a0cdc3798108863a3e27b894fc467b7ca80b
Jhon Rey <jhonreymendiola@gmail.com>
2023-05-31 14:22:30 +0800

    header part with image

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
-
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    MatIconModule
   ]
 })
 export class LandingModule { }
