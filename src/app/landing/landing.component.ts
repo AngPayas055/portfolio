fae515e0eacc040fb264eeeb8bba2b5a39d4cbe5
Jhon Rey <jhonreymendiola@gmail.com>
2023-06-05 01:25:28 +0800

    minor changes

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
+  warning:boolean = false
 
+  openWraning(){
+    if(this.warning == true){
+       this.warning = false
+       return
+    }
+    this.warning = true;
+    setTimeout(() => {
+      this.warning = false;
+    }, 2000);
+  }
 }
